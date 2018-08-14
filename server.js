b1625dac09acba2bb40cb825ac21dc66f5d54dc6
PhilB56 <philb56@users.noreply.github.com>
2018-08-14 19:05:13 +0100

    Create the User Model User Registration Email & Password Logon Creating the JWT Passport JWT Authentication Strategy

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const profile = require("./routes/api/profile");
 const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//Body Parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //db config
 const db = require("./config/keys").mongoURI;
@@ -19,7 +25,11 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
 
 //Routes
 app.use("/api/posts", posts);
